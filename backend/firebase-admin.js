ccd7c87ff43cac8441761a4c2fcd7f78cd8af587
Ayijuka Ernest <your-github-email@example.com>
2026-06-11 18:35:22 +0300

    Add GitHub Actions automation

--- backend/firebase-admin.js
+++ backend/firebase-admin.js
@@ -1,7 +1,9 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
-const serviceAccount = require("./ishaka-online-market-firebase-adminsdk-fbsvc-986c704df8.json");
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 initializeApp({
   credential: cert(serviceAccount)
@@ -12,4 +14,3 @@ const db = getFirestore();
 module.exports = {
   db
 };
-
